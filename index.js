4a34f20baddf8a676402ad7543fea86c1f695536
Fabian Krutsch <fabian.krutsch@publicissapient.com>
2023-05-31 14:02:29 +0200

    fix: cjs to esm

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import StyleDictionaryModule from "style-dictionary";
 import { makeSdTailwindConfig } from "sd-tailwindcss-transformer";
-import { rm } from "fs/promises";
+import { rm, readFile, writeFile } from "fs/promises";
+
+// Base Config
+const out = "build";
+const buildPathTW = `${out}/tailwind/`;
 
 const config = {
   source: ["tokens.json"],
   platforms: {
     css: {
       transformGroup: "css",
-      buildPath: "build/css/",
+      buildPath: `${out}/css/`,
       files: [
         {
           destination: "variables.css",
@@ -18,14 +22,23 @@ const config = {
   },
 };
 
+// Extend Config with Tailwind
 const tailwind = makeSdTailwindConfig({
   type: "all",
-  buildPath: "build/tailwind/",
+  buildPath: buildPathTW,
 });
-
 config.format = tailwind.format;
 config.platforms.tailwind = tailwind.platforms.tailwind;
 
-await rm("build", { force: true, recursive: true });
+// Delete folder and generate files
+await rm(out, { force: true, recursive: true });
 const StyleDictionary = StyleDictionaryModule.extend(config);
 StyleDictionary.buildAllPlatforms();
+
+// Modify TW config from CJS to ESM export
+const rewriteFile = `${buildPathTW}tailwind.config.js`;
+const configTWFile = await readFile(rewriteFile, "utf-8");
+await writeFile(
+  rewriteFile,
+  configTWFile.replace("module.exports = ", "export default ")
+);
